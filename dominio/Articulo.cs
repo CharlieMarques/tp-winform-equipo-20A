39cd4da7e6fe29ab606fe82ef562729da209468b
matiasmarto06 <matiasmarto06@gmail.com>
2024-09-03 19:26:59 -0300

    agrego propiedades de clase Articulo

--- dominio/Articulo.cs
+++ dominio/Articulo.cs
@@ -8,6 +8,19 @@ namespace Dominio
 {
     public class Articulo
     {
-        //TODO: Propiedades y metodos
+        // Propiedades
+        public int Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public Marca Marca { get; set; }
+        public Categoria Categoria { get; set; }
+        public Imagen Imagen { get; set; }
+        public float Precio { get; set; }
+
+        // Metodos
+        public override string ToString()
+        {
+            return Descripcion;
+        }
     }
 }
