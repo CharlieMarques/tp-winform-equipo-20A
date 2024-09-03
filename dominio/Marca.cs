a2e8fe8f8d5751e0be1c3651104bf527e0eb7db9
Diego Leonel Luque <diego.leonel.luque@gmail.com>
2024-09-03 20:46:57 -0300

    constructores para iniciar con descripcion, elimine set id por el momento

--- dominio/Marca.cs
+++ dominio/Marca.cs
@@ -8,7 +8,12 @@ namespace Dominio
 {
     public class Marca
     {
-        public int Id { get; set; }
+        public Marca(string descripcion) // quizas agregar int id, si la base no lo hace autonumerico
+        {
+            Descripcion = descripcion;
+        }
+        public Marca() { }
+        public int Id { get; } //el id es autonimerico en la base de datos no deberia de tener set, en todo caso un metodo que lo convierta en autonumerico
         public string Descripcion { get; set; }
         public override string ToString()
         {
