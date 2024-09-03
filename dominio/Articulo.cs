fef4ecc402e57ed68b602d9ffc5b7c94fe8c2c76
Diego Leonel Luque <diego.leonel.luque@gmail.com>
2024-09-03 20:43:48 -0300

    agregado get de id y corregido tipo en propiedad codigo

--- dominio/Articulo.cs
+++ dominio/Articulo.cs
@@ -9,7 +9,8 @@ namespace Dominio
     public class Articulo
     {
         // Propiedades
-        public int Codigo { get; set; }
+        public int Id { get; } // el id es autonumerico en la base de datos
+        public string Codigo { get; set; } // el codigo contiene numeros y letras
         public string Nombre { get; set; }
         public string Descripcion { get; set; }
         public Marca Marca { get; set; }
