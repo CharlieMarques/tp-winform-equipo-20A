62c6389a8c5179da16072c478ea5eaac2fb03978
Diego Leonel Luque <diego.leonel.luque@gmail.com>
2024-09-03 20:32:30 -0300

    implementado grid de articulos, busqueda por codigo

--- winform-equipo-20A/frmArticulos.Designer.cs
+++ winform-equipo-20A/frmArticulos.Designer.cs
@@ -28,13 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.buttonBusqueda = new System.Windows.Forms.Button();
+            this.textBoxBusqueda = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(64, 202);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(851, 333);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // buttonBusqueda
+            // 
+            this.buttonBusqueda.Location = new System.Drawing.Point(304, 41);
+            this.buttonBusqueda.Name = "buttonBusqueda";
+            this.buttonBusqueda.Size = new System.Drawing.Size(75, 23);
+            this.buttonBusqueda.TabIndex = 1;
+            this.buttonBusqueda.Text = "Buscar";
+            this.buttonBusqueda.UseVisualStyleBackColor = true;
+            // 
+            // textBoxBusqueda
+            // 
+            this.textBoxBusqueda.Location = new System.Drawing.Point(64, 43);
+            this.textBoxBusqueda.Name = "textBoxBusqueda";
+            this.textBoxBusqueda.Size = new System.Drawing.Size(234, 20);
+            this.textBoxBusqueda.TabIndex = 2;
+            this.textBoxBusqueda.TextChanged += new System.EventHandler(this.textBoxBusqueda_TextChanged);
+            // 
+            // frmArticulos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1066, 640);
+            this.Controls.Add(this.textBoxBusqueda);
+            this.Controls.Add(this.buttonBusqueda);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "frmArticulos";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button buttonBusqueda;
+        private System.Windows.Forms.TextBox textBoxBusqueda;
     }
 }
 
