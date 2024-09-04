d2aa135503df74547c77d7c03e52cb35506c4d99
Diego Leonel Luque <diego.leonel.luque@gmail.com>
2024-09-04 17:20:42 -0300

    metodo agregar para agregar articulo

---
+++ Catalogo-Comercio/artículoBD.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Catalogo_Comercio;
+using System.Xml.Linq;
+using Dominio;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Catalogo_Comercio
+{
+    internal class artículoBD
+    {
+        PublicKey void agregar(Articulo nuevo)
+        {
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setConsulta("Insert Into ARTICULOS (Id, Codigo, Nombre, Descripcion, Marca, Categoria, Imagen, Precio) Values ('" + nuevo.Id + "', '" + nuevo.Codigo + "', '" + nuevo.Nombre + "', '" + nuevo.Descripcion + "', '" + nuevo.Marca + "', '" + nuevo.Categoria + "', '" + nuevo.Imagen + "', '" + nuevo.Precio + "')");
+                datos.ejecutarConsulta();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+    }
+}
